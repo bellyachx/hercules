f730c8fc4bba989c736125ffb89524c6825bb2c8
Maxim <maximcepraga39@gmail.com>
2025-03-14 23:32:31 +0200

    HC-11/auth
    Disable security for actuator.

--- src/main/java/me/maxhub/hercules/config/SecurityConfig.java
+++ src/main/java/me/maxhub/hercules/config/SecurityConfig.java
@@ -16,7 +16,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+        return httpSecurity.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwt ->
                                 jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
